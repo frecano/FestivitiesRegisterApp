f4d1a3181d05e545f0812173d46b625efe3d884f
frecano <frecano@gmail.com>
2016-04-03 15:14:11 -0500

    Basic Structure of the project

---
+++ src/main/java/com/festivities_register/FestivitiesRegisterApplication.java
@@ -0,0 +1,11 @@
+package com.festivities_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivitiesRegisterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FestivitiesRegisterApplication.class, args);
+	}
+}
